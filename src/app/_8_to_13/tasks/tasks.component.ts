7d1d809bfe7c343f1d3f7493d7ff202b267269ee
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-10-08 20:25:16 +0500

    12. Injecting Services Into Services

--- src/app/_8_to_13/tasks/tasks.component.ts
+++ src/app/_8_to_13/tasks/tasks.component.ts
@@ -18,6 +18,31 @@
 
 
 //#region 10. Providing Services via the Element Injector
+// import { Component } from '@angular/core';
+
+// import { NewTaskComponent } from './new-task/new-task.component';
+// import { TasksListComponent } from './tasks-list/tasks-list.component';
+// import { TasksService } from './tasks.service';
+
+// @Component({
+//   selector: 'app-tasks',
+//   standalone: true,
+//   templateUrl: './tasks.component.html',
+//   imports: [NewTaskComponent, TasksListComponent],
+//   //======== Element Injector =======
+//   // this TasksService only injected in   Task and its child components
+//   // so this will not work in App Component 
+//   // BUG this is drop back of   Element injector
+//   // BUG this does not changes Multiple instance of same Component
+  
+//   providers: [TasksService]
+// })
+// export class TasksComponent {}
+
+//#endregion
+
+
+//#region 12. Injecting Services Into Services
 import { Component } from '@angular/core';
 
 import { NewTaskComponent } from './new-task/new-task.component';
@@ -29,13 +54,6 @@ import { TasksService } from './tasks.service';
   standalone: true,
   templateUrl: './tasks.component.html',
   imports: [NewTaskComponent, TasksListComponent],
-  //======== Element Injector =======
-  // this TasksService only injected in   Task and its child components
-  // so this will not work in App Component 
-  // BUG this is drop back of   Element injector
-  // BUG this does not changes Multiple instance of same Component
-  
-  providers: [TasksService]
 })
 export class TasksComponent {}
 
--- src/app/_8_to_13/tasks/tasks.service.ts
+++ src/app/_8_to_13/tasks/tasks.service.ts
@@ -32,19 +32,59 @@
 
 
 //#region 8. Angular Has Multiple Injectors!
-import { Injectable, signal } from '@angular/core';
+// import { Injectable, signal } from '@angular/core';
+// import { Task, TaskStatus } from './task.model';
+
+// //========= 1. 8. Angular Has Multiple Injectors!root Inject  ==========
+// // if i remove this will give error and go to
+// // ERROR NullInjectorError: R3InjectorError(Standalone[_AppComponent])[TasksService -> TasksService -> TasksService]: 
+
+// // @Injectable({
+// //   providedIn: 'root',
+// // })
+
+
+// export class TasksService {
+//   private tasks = signal<Task[]>([]);
+//   allTasks = this.tasks.asReadonly();
+
+//   addTask(taskData: { title: string; description: string }) {
+//     const newTask: Task = {
+//       ...taskData,
+//       id: Math.random().toString(),
+//       status: 'OPEN',
+//     };
+//     this.tasks.update((oldTasks) => [...oldTasks, newTask]);
+//   }
+
+//   updateTaskStatus(taskId: string, newStatus: TaskStatus) {
+//     this.tasks.update((oldTasks) =>
+//       oldTasks.map((task) =>
+//         task.id === taskId ? { ...task, status: newStatus } : task
+//       )
+//     );
+//   }
+// }
+//#endregion
+
+
+
+
+//#region 12. Injecting Services Into Services
+import { inject, Injectable, signal } from '@angular/core';
 import { Task, TaskStatus } from './task.model';
+import { LoggingService } from '../logging.service';
 
 //========= 1. 8. Angular Has Multiple Injectors!root Inject  ==========
 // if i remove this will give error and go to
 // ERROR NullInjectorError: R3InjectorError(Standalone[_AppComponent])[TasksService -> TasksService -> TasksService]: 
 
-// @Injectable({
-//   providedIn: 'root',
-// })
-
-
+@Injectable({
+  providedIn: 'root',
+})
 export class TasksService {
+  private loggingService = inject(LoggingService);
+
   private tasks = signal<Task[]>([]);
   allTasks = this.tasks.asReadonly();
 
@@ -55,6 +95,8 @@ export class TasksService {
       status: 'OPEN',
     };
     this.tasks.update((oldTasks) => [...oldTasks, newTask]);
+
+    this.loggingService.log("Added Task with Title " + taskData.title);
   }
 
   updateTaskStatus(taskId: string, newStatus: TaskStatus) {
@@ -63,9 +105,9 @@ export class TasksService {
         task.id === taskId ? { ...task, status: newStatus } : task
       )
     );
+
+    
+    this.loggingService.log("Change Task Status to " + newStatus);
   }
 }
 //#endregion
-
-
-
