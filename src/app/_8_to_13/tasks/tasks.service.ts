88ea09a52fc5203efcd34b75e8570982e1c2e43f
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-10-08 19:12:14 +0500

    8. Angular Has Multiple Injectors!

--- src/app/_8_to_13/tasks/tasks.service.ts
+++ src/app/_8_to_13/tasks/tasks.service.ts
@@ -35,7 +35,7 @@
 import { Injectable, signal } from '@angular/core';
 import { Task, TaskStatus } from './task.model';
 
-//========= root Inject  ==========
+//========= 1. root Inject  ==========
 // if i remove this will give error and go to
 // ERROR NullInjectorError: R3InjectorError(Standalone[_AppComponent])[TasksService -> TasksService -> TasksService]: 
 
