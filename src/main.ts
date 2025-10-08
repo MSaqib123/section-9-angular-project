2d7ff8a5fb41f57d93f9cfa36ad92f84354bfdbc
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-10-08 19:30:25 +0500

    9. There Are Multiple Ways Of Providing a Service

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,33 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+//============================================
+//=========== _1_to_7 ================
+//============================================
+//#region 
 
-bootstrapApplication(AppComponent, appConfig)
+// import { bootstrapApplication } from '@angular/platform-browser';
+// import { appConfig } from './app/app.config';
+// import { AppComponent } from './app/app.component';
+
+// bootstrapApplication(AppComponent, appConfig)
+//   .catch((err) => console.error(err));
+
+//#endregion
+
+
+
+
+//============================================
+//=========== _8_to_13 ================
+//============================================
+//#region 9. There Are Multiple Ways Of Providing a Service
+
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { TasksService } from './app/_8_to_13/tasks/tasks.service';
+
+bootstrapApplication(AppComponent,{
+  // this does not allow  Tree shaking
+  // this will be used everytime , means this is not good for performance way
+  providers: [TasksService]
+})
   .catch((err) => console.error(err));
+//#endregion
