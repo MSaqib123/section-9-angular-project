6dc3bbd9a064530209de4920d2443064e05bf696
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-10-08 18:43:18 +0500

    7. Outsourcing & Reusing Logic with Services

--- src/app/_1_to_7/tasks/tasks-list/task-item/task-item.component.ts
+++ src/app/_1_to_7/tasks/tasks-list/task-item/task-item.component.ts
@@ -1,7 +1,60 @@
-import { Component, computed, input } from '@angular/core';
+//#region  Class #2
+// import { Component, computed, input } from '@angular/core';
+// import { FormsModule } from '@angular/forms';
+
+// import { Task, TaskStatus } from '../../task.model';
+
+// @Component({
+//   selector: 'app-task-item',
+//   standalone: true,
+//   imports: [FormsModule],
+//   templateUrl: './task-item.component.html',
+//   styleUrl: './task-item.component.css',
+// })
+// export class TaskItemComponent {
+//   task = input.required<Task>();
+//   taskStatus = computed(() => {
+//     switch (this.task().status) {
+//       case 'OPEN':
+//         return 'Open';
+//       case 'IN_PROGRESS':
+//         return 'Working on it';
+//       case 'DONE':
+//         return 'Completed';
+//       default:
+//         return 'Open';
+//     }
+//   });
+
+//   onChangeTaskStatus(taskId: string, status: string) {
+//     let newStatus: TaskStatus = 'OPEN';
+
+//     switch (status) {
+//       case 'open':
+//         newStatus = 'OPEN';
+//         break;
+//       case 'in-progress':
+//         newStatus = 'IN_PROGRESS';
+//         break;
+//       case 'done':
+//         newStatus = 'DONE';
+//         break;
+//       default:
+//         break;
+//     }
+//   }
+// }
+
+//#endregion
+
+
+//#region  7. Outsourcing & Reusing Logic with Services
+
+import { Component, computed, inject, input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { Task, TaskStatus } from '../../task.model';
+import { TasksService } from '../../tasks.service';
 
 @Component({
   selector: 'app-task-item',
@@ -11,6 +64,8 @@ import { Task, TaskStatus } from '../../task.model';
   styleUrl: './task-item.component.css',
 })
 export class TaskItemComponent {
+  private tasksService = inject(TasksService);
+
   task = input.required<Task>();
   taskStatus = computed(() => {
     switch (this.task().status) {
@@ -41,5 +96,7 @@ export class TaskItemComponent {
       default:
         break;
     }
+    this.tasksService.updateTaskStatus(taskId,newStatus)
   }
 }
+//#endregion
--- src/app/_1_to_7/tasks/tasks.service.ts
+++ src/app/_1_to_7/tasks/tasks.service.ts
@@ -2,13 +2,11 @@
 // import { Injectable, signal } from "@angular/core";
 // import { Task } from "./task.model";
 
-
 // @Injectable({
 //     providedIn:'root'
 // })
 // export class TasksService{
 //     tasks = signal<Task[]>([]);
-
 
 //     addTask(taskData:{title:string; description:string}){
 //         const newTask:Task = {
@@ -21,29 +19,55 @@
 // }
 //#endregion
 
-
-
 //#region 6. Using The Alternative Dependency Injection Syntax
-import { Injectable, signal } from "@angular/core";
-import { Task } from "./task.model";
+// import { Injectable, signal } from "@angular/core";
+// import { Task } from "./task.model";
 
+// @Injectable({
+//     providedIn:'root'
+// })
+// export class TasksService{
+
+//     private tasks = signal<Task[]>([]);
+//     allTasks = this.tasks.asReadonly();
+
+//     addTask(taskData:{title:string; description:string}){
+//         const newTask:Task = {
+//             ...taskData,
+//             id:Math.random().toString(),
+//             status:'OPEN'
+//         }
+//         this.tasks.update((oldTasks)=> [...oldTasks,newTask] )
+//     }
+// }
+//#endregion
+
+//#region 7. Outsourcing & Reusing Logic with Services
+import { Injectable, signal } from '@angular/core';
+import { Task, TaskStatus } from './task.model';
 
 @Injectable({
-    providedIn:'root'
+  providedIn: 'root',
 })
-export class TasksService{
-    
-    private tasks = signal<Task[]>([]);
-    allTasks = this.tasks.asReadonly();
+export class TasksService {
+  private tasks = signal<Task[]>([]);
+  allTasks = this.tasks.asReadonly();
 
+  addTask(taskData: { title: string; description: string }) {
+    const newTask: Task = {
+      ...taskData,
+      id: Math.random().toString(),
+      status: 'OPEN',
+    };
+    this.tasks.update((oldTasks) => [...oldTasks, newTask]);
+  }
 
-    addTask(taskData:{title:string; description:string}){
-        const newTask:Task = {
-            ...taskData,
-            id:Math.random().toString(),
-            status:'OPEN'
-        }
-        this.tasks.update((oldTasks)=> [...oldTasks,newTask] )
-    }
+  updateTaskStatus(taskId: string, newStatus: TaskStatus) {
+    this.tasks.update((oldTasks) =>
+      oldTasks.map((task) =>
+        task.id === taskId ? { ...task, status: newStatus } : task
+      )
+    );
+  }
 }
 //#endregion
